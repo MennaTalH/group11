a55aa586d134c24f6ab9a3b121128113da868956
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2022-02-06 16:12:34 +0200

    session1

---
+++ index.php
@@ -0,0 +1,168 @@
+<?php 
+
+# print message      single line comment (# ,  // )  
+
+
+/*   multi lines comment 
+test  
+message 
+*/
+
+// echo "test Message";
+
+
+
+// $age = 20;
+
+// $name = "test";
+
+// echo $name;
+
+
+// var_dump($age);
+// var_dump($name);
+
+
+
+// $age2 = 30;
+
+// ECHO $age;     //  30 
+
+
+
+/*
++ 
+-
+* 
+/ 
+% 
+**
+*/
+
+// $num1 = 20;
+// $num2 = "20";
+
+//  echo  $num1+$num2;
+
+// $name = "ahmed";
+
+
+// echo $num1 += 3;  //   $num1 =  $num1+3
+
+
+  //var_dump($num1 ==  $num2);   // true   
+  //  var_dump($num1 ===  $num2);   // false    
+
+
+//    $num1 +=1;
+// echo $num1;
+
+
+  //  echo $num1--;     // $num1+1;
+
+   // echo $num1;
+
+
+/*
+     &&
+    Cond1      op       Cond2     Result 
+     true      AND      true        true 
+     true      And      false       false 
+     false     and      true        false 
+     false     and      false       false 
+
+
+
+     || 
+     Cond1     op         Cond2     Result 
+     true      or         true       true 
+     true      or         false      true 
+     false     or         true       true 
+     false     or         false      false  
+
+
+
+     Cond1     op         Cond2     Result 
+     true      xor         true       false 
+     true      xor         false      true
+     false     xor         true       true
+     false     xor         false      false  
+
+
+
+     !   not 
+*/
+
+//    var_dump(false XOR true);
+
+
+//  $text = "php";
+
+// //  echo $text." course";
+
+// echo $text .= "course";
+
+
+
+
+
+//    $age = 20;
+//    $age = 21;
+
+//    echo $age ; 
+
+//  define("size",50);
+
+//    const size = 50;
+
+//     // echo size;
+
+// echo constant('size');
+
+
+
+
+   
+
+
+  // 
+
+
+
+
+
+//    if(condition){
+//        // code ..... 
+//    }
+
+
+// $status = 0;
+
+
+// if($status == 1){
+//     echo 'welcome to our website ';
+// }else{
+
+//     echo 'You should Login First';
+// }
+
+
+
+$age = 15; 
+
+
+if($age <= 10){
+
+    echo 'Age <= 10';
+}elseif($age >10 && $age <=20){
+    echo "age between 10 , 20";
+}else{
+    echo "age > 20";
+}
+
+
+
+
+
+?>
+
