207133ee1c1f59109fa716300b991677da204a15
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2022-02-13 15:22:26 +0200

    Abdelkareem

---
+++ week2/session1.php
@@ -0,0 +1,69 @@
+<?php 
+
+
+
+//   date(format , timestamp );
+/*
+d 01 - 31 
+D sun 
+m 01 - 12 
+M jan 
+F full Month Name ... 
+y 22  
+Y 2022
+***************************** 
+
+h  01-12 
+H  00:23
+i  00:59 
+s  00:59
+a  am,pm
+A  AM,PM 
+*/ 
+
+
+//  echo   date_default_timezone_get();
+// date_default_timezone_set('africa/cairo');
+
+// echo date('Y-m-d h:i:s a ').'<br>';
+
+// //  echo date('Y'); 
+
+// echo time();
+
+   // 2021/3/3 
+   //echo strtotime('3-3-2021   9:10 am');    // 1614759000 
+  //  echo  date('d/M,Y  h:i:s a',1614759000);
+
+
+/*
+
+users  [name ,email,password] 
+Blog   [title,content,image]
+
+
+
+
+User Table  
+id    name      email       password         
+ 1     x           x@x        123
+ 2     y           y@y        456 
+
+
+
+
+Blog Table 
+title    content      image
+
+
+
+
+
+*/
+
+
+
+
+
+
+?>
